042fc479ad926fc79348c36de16d6bc713c2ddee
Felipe Alencar <fsalencar@outlook.com>
2017-01-17 09:24:56 -0200

    Adicionando o filtro por AE

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -2,8 +2,25 @@
 
 angular.module('LivroMean').controller('ContatosController',
   function($scope){
+
             $scope.total = 0;
             $scope.incrementa = function() {
               $scope.total++;
             };
+            $scope.contatos = [
+              {"_id": 1,
+              "nome":"Jhontinha Noob",
+              "email":"jhontinha@stefanini.com"
+              },
+              {"id":2,
+              "nome":"Gaucho",
+              "email":"capoeiraDeGaucho@gmail.com"
+            },
+            {"id":3,
+            "nome":"Zenon",
+            "email":"SenhorBarriga@gmail.com"
+          },
+        ];
+        $scope.filtro=''; //Declara o filtro da html contatos para utilizar a propriedade FILTER na página
+
 });
